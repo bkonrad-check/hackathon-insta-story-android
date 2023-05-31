44b66cfafb0f4c3a3cab95734a05cbdd23adc452
viktor wart <viktor.wart@c24.de>
2023-05-31 16:11:01 +0200

    ended a hardcoded URL to a streamable video locally

--- app/src/main/java/de/check24/hackathon/instagramstory/network/DataSource.kt
+++ app/src/main/java/de/check24/hackathon/instagramstory/network/DataSource.kt
@@ -1,6 +1,8 @@
 package de.check24.hackathon.instagramstory.network
 
+import de.check24.hackathon.instagramstory.mod.ChapterApi
 import de.check24.hackathon.instagramstory.mod.Storries
+import de.check24.hackathon.instagramstory.mod.Story
 import retrofit2.Response
 
 class DataSource(
@@ -9,10 +11,33 @@ class DataSource(
     suspend fun stories(): Storries {
         val response: Response<Storries> = apiService.stories()
         if (response.isSuccessful) {
-            return requireNotNull(response.body())
+            return Storries(
+                stories = (response.body()?.stories?.toMutableList() ?: mutableListOf()).also {
+                    it.add(
+                        0, Story(
+                            chapters = listOf(
+                                ChapterApi(
+                                    listOf(),
+                                    null,
+                                    13377,
+                                    44000,
+                                    1,
+                                    null,
+                                    "",
+                                    "VIDEO",
+                                    "https://cdn.c24.de/city-3840x2160-24fps/master.m3u8"
+                                )
+                            ),
+                            13377,
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Empire_State_Building_Aerial.JPG/1280px-Empire_State_Building_Aerial.JPG",
+                            "this is the title"
+                        )
+                    )
+                })
         }
         throw NetworkException(response.code(), response.errorBody().toString())
     }
 
 }
+
 class NetworkException(val code: Int, val errorBody: String) : Throwable()
